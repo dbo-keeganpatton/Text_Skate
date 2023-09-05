c72cb03e3f19ebd81e29a46f9637e48071b5f969
Keegan Patton <keeganpatton@gmail.com>
2023-09-04 19:32:08 -0500

    Game of SKATE v0.2: added ASCII animations. Fixed flow control issues..

--- play.py
+++ play.py
@@ -1,47 +1,49 @@
 import random
 import pandas as pd
 
-df = pd.read_csv('flip_tricks.csv')
-tricks = df.values.tolist()
+tricks = pd.read_csv('flip_tricks.csv').values.tolist()
+success = open('board.txt').read()
+failure = open('broken_brd.txt').read()
+ask_to_play = open('opponent.txt').read()
+
 
 game_player = []
 game_opponent = []
 letters = ['S', 'K', 'A', 'T', 'E']
 attempt = ['land', 'bail']
 
-
 def question():
-    '''Ask the Player to play and terminate the program if no(n)'''
-    ask_player = input('Wanna play a game of SKATE? (y/n)\n')
+    '''Ask to play, terminate the program if no (n)'''
+    ask_player = input('(y/n)\n')
     if ask_player.lower() == 'y':
         print('Sick!')
     else:
         print('Ok maybe next time!')
         exit()
 
-
 def action():
-    '''Who starts the game?'''
     if random.choice(attempt) == 'land':
-        return True  # Player starts
+        return True  
     else:
-        return False  # Computer starts
-
+        return False  
 
 
 def play():
-    
+    '''Main Program'''
+    print(ask_to_play)
     question()
+    
+    current_turn = action()  # Who sets, True for Player
 
-    while len(game_player) < len(letters) and len(game_opponent) < len(letters):
-
-        player_action = action()
-        cpu_action = action()
-
-        if player_action:
+    while len(game_player) < len(letters) and len(game_opponent) < len(letters): 
+        
+        if current_turn:  # Player turn
             input("Set the Trick: (Type Trick)   \n")
+            player_action = action()
             if player_action:
+                print(success)
                 print("Stomped it! \nCPU tries the trick")
+                cpu_action = action()
                 if cpu_action:
                     print("CPU Lands your trick")
                 else:
@@ -49,15 +51,18 @@ def play():
                     game_opponent.append(letters[len(game_opponent)])
                     print(f'CPU Score: {game_opponent}')
             else:
+                print(failure)
                 print("You bailed")
-        
-        else:
+                current_turn = not current_turn
+            
+        else:  # CPU turn
 
             print("CPU sets the Trick:  \n")
-
+            cpu_action = action()
             if cpu_action:
                 print(f"CPU lands a {random.choice(tricks)}")
                 input("( Type trick CPU does to attempt ) ...\n")
+                player_action = action()
                 if player_action:
                     print("You Landed it")
                 else:
@@ -66,8 +71,8 @@ def play():
                     print(game_player)
             else:
                 print("CPU bails trick")
-            
-                        
+                current_turn = not current_turn  # Change turn to Player
+
 
     if len(game_player) == len(letters):
         print("You Lose")
@@ -76,6 +81,5 @@ def play():
     if len(game_opponent) == len(letters):
         print("You Win!")
         exit()
-        
 
 play()
